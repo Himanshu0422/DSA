348e68b63ce761e45f1c7c42e82889b9f2079392
Himannshu121203 <himanshu1571.be20@chitkara.edu.in>
2022-03-31 08:54:32 +0530

    1

---
+++ DSA/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello"<<endl;
+
+    int a, b;
+    cin>>a>>b;
+    int sum = a+b;
+    cout<<"sum"<<endl;
+    cout<<sum<<endl;
+    return 0;
+}
---
+++ DSA/ifelse.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    /* greater of 3 */
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    if(a>b){
+        if(a>c){
+            cout<<a<<endl;
+        }
+        else{
+            cout<<c<<endl;
+        }
+    }
+    else{
+        if(b>c){
+            cout<<b<<endl;
+        }else{
+            cout<<c<<endl;
+        }
+    }
+
+    /* Odd Even */
+    int n;
+    cin>>n;
+    if(n%2 == 0){ 5
+        cout<<"even"<<endl;
+    }else{
+        cout<<"odd"<<endl;
+    }
+
+
+    return 0;
+}
