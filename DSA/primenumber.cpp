48308b091c1c0b27a4816db5bc7d6a5186c6eb6c
Himannshu121203 <himanshu1571.be20@chitkara.edu.in>
2022-04-14 18:24:02 +0530

    prime

--- DSA/primenumber.cpp
+++ DSA/primenumber.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<cmath>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -7,8 +7,9 @@ int main()
     int n;
     cin >> n;
 
+    /*
     bool flag = 0;
-    for (int i = 2; i < sqrt(n); i++)
+    for (int i = 2; i <= sqrt(n); i++)
     {
         if (n % i == 0)
         {
@@ -20,6 +21,35 @@ int main()
     if (flag == 0)
     {
         cout << "Prime" << endl;
+    }*/
+
+    /* Reverse Number*/
+
+    /*  int reverse;
+while(n>0){
+      int lastdigit=n%10;
+      reverse =reverse*10 + lastdigit;
+      n= n/10;
+}
+cout<<reverse<<endl;*/
+
+    /* Armstrong */
+
+    int sum = 0;
+    int original = n;
+    while (n > 0)
+    {
+        int lastdigit = n % 10;
+        sum += pow(lastdigit, 3);
+        n = n / 10;
+    }
+    if (sum == original)
+    {
+        cout << "Armstrong Number" << endl;
+    }
+    else
+    {
+        cout << "Not" << endl;
     }
     return 0;
 }
