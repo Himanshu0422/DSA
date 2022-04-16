9efcc53ff3e15c35c247ca34a440ce0d4e30eb76
Himannshu121203 <himanshu1571.be20@chitkara.edu.in>
2022-04-16 13:33:23 +0530

    46541

---
+++ DSA/arrays/intro.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // int n;
+    // cin >> n;
+    // int arr[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int maxNO = INT_MIN;
+    int minNO = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        maxNO = max(maxNO,arr[i]);
+        minNO = min(minNO,arr[i]);
+    }
+    cout<<maxNO<<minNO;
+    return 0;
+}
