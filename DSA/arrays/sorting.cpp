242fe12c11763ecc74896fc90fe4f8cabedccada
Himannshu121203 <himanshu1571.be20@chitkara.edu.in>
2022-04-16 22:49:40 +0530

    1

--- DSA/arrays/sorting.cpp
+++ DSA/arrays/sorting.cpp
@@ -10,11 +10,48 @@ void selectionSort(int arr[], int n)
         {
             if (arr[j] < arr[i])
             {
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+                swap(arr[j], arr[i]);
             }
         }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+void bubbleSort(int arr[], int n)
+{
+    int counter = 1;
+    while (counter < n)
+    {
+        for (int i = 0; i < n - counter; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                swap(arr[i], arr[i + 1]);
+            }
+        }
+        counter++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+void insertSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int current = arr[i];
+        int j = i - 1;
+        while (arr[j] > current && j >= 0)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
     }
     for (int i = 0; i < n; i++)
     {
@@ -32,7 +69,10 @@ int main()
         cin >> arr[i];
     }
 
-
-    selectionSort(arr,n);
+    selectionSort(arr, n);
+    cout << endl;
+    bubbleSort(arr, n);
+    cout << endl;
+    insertSort(arr, n);
     return 0;
 }
