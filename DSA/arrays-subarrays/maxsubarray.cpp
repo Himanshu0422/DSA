29774a0b4f09f5f59af075e10d7882c68c339a68
Himannshu121203 <himanshu1571.be20@chitkara.edu.in>
2022-04-22 22:56:35 +0530

    85

---
+++ DSA/arrays-subarrays/maxsubarray.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int bruteforce(int arr[], int n)
+{
+    int maxSum = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            int sum = 0;
+            for (int k = i; k <= j; k++)
+            {
+                sum += arr[k];
+            }
+            maxSum = max(maxSum, sum);
+        }
+    }
+    return maxSum;
+}
+
+int cumulativesumarray(int arr[], int n)
+{
+    int currsum[n + 1];
+    currsum[0] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        currsum[i] = currsum[i - 1] + arr[i - 1];
+    }
+
+    int maxSum = INT_MIN;
+    for (int i = 1; i <= n; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < i; j++)
+        {
+            sum = currsum[i] - currsum[j];
+            maxSum = max(sum, maxSum);
+        }
+    }
+    return maxSum;
+}
+
+int kadane(int arr[], int n)
+{
+    int currentSum = 0;
+    int maxSum = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        currentSum += arr[i];
+        if (currentSum < 0)
+        {
+            currentSum = 0;
+        }
+        maxSum = max(maxSum, currentSum);
+    }
+    return maxSum;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << bruteforce(arr, n) << endl;
+    cout << cumulativesumarray(arr, n) << endl;
+    cout << kadane(arr, n) << endl;
+    return 0;
+}
