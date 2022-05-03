5b28966fc6e03c4c41ecbe75fae31276679a2225
Himannshu121203 <himanshu1571.be20@chitkara.edu.in>
2022-05-03 18:40:27 +0530

    dj

---
+++ DSA/Srings/challanges.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    /*string str = "adjwurjsdfwsf";
+
+    // covert to upper case
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] -= 32;
+        }
+    }
+
+    cout << str << endl;*/
+
+    /*string s = "dtfgvdfcgvsed";
+
+    transform(s.begin(), s.end(), s.begin(), ::toupper);
+    cout << s << endl;*/
+
+    // form the biggest number from the numeric string
+    /*string s = "548545455";
+
+    sort(s.begin(), s.end(), greater<int>());
+
+    cout << s << endl;*/
+
+    // find max appearing letter
+
+    string s = "dfgvsrdhgvdsfbsrd";
+
+    int freq[26];
+    for (int i = 0; i < 26; i++)
+    {
+        freq[i] = 0;
+    }
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        freq[s[i] - 'a']++;
+    }
+
+    char ans = 'a';
+    int maxF = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq[i] > maxF)
+        {
+            maxF = freq[i];
+            ans = i + 'a';
+        }
+    }
+
+    cout << maxF << " " << ans << endl;
+
+    return 0;
+}
---
+++ DSA/Srings/intro.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    /*string str1(5, 'n');
+    string str;
+    getline(cin, str); //for using line without getline only first word is used
+    cout<<str<<endl;*/
+
+    // appending string
+    /*string s1 = "fam";
+    string s2 = "ily";
+
+    s1.append(s2);
+    cout << s1 << endl;
+    // cout << s1 + s2 << endl; another way of appending string
+
+    // accessing character
+
+    cout << s1[1] << endl;
+    */
+
+    /*string abc = "eitdghetsidhiutdhg";
+    abc.clear();
+    cout << abc << endl;*/
+
+    /*string s1 = "abc";
+    string s2 = "abc";
+
+    // cout << s2.compare(s1) << endl; +ve for s2 = xyz, s1 = abc
+
+    if (s1.compare(s2) == 0)
+    {
+        cout << "strings are equal";
+    }*/
+
+    // string s1 = "nincompoop";
+    /*s1.clear();
+
+    if (s1.empty())
+    {
+        cout << "string is empty" << endl;
+    }*/
+
+    /*s1.erase(3, 3);
+    cout << s1 << endl;*/
+
+    // cout << s1.find("com") << endl;
+
+    /*s1.insert(2, "lol");
+    cout << s1 << endl;*/
+
+    // cout << s1.size() << endl; size/length
+
+    /*string s = s1.substr(6, 4);
+    cout << s << endl;*/
+
+    /*string s1 = "786";
+    int x = stoi(s1);
+    cout << x + 2 << endl;*/
+
+    /*int x = 786;
+
+    cout << to_string(x) + "2" << endl;*/
+
+    string s1 = "xcmnmvccxmfas";
+
+    sort(s1.begin(), s1.end());
+
+    cout << s1 << endl;
+
+    return 0;
+}
