1addacf768a14690f53fa491ddd821933fcd5d1c
Himannshu121203 <himanshu1571.be20@chitkara.edu.in>
2022-10-08 14:38:50 +0530

    d

---
+++ Web Development/react-prac/src/index.js
@@ -0,0 +1,6 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>HLO!</h1>, document.getElementById("root"));
