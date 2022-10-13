b6e3782a774eba0337e91a6d2f85fa5551dfbc9c
Himannshu121203 <himanshu1571.be20@chitkara.edu.in>
2022-10-13 12:40:49 +0530

    cpp

--- DSA/arrays-subarrays/intro.cpp
+++ DSA/arrays-subarrays/intro.cpp
@@ -16,22 +16,23 @@ int main()
     //     cout << arr[i] << " ";
     // }
 
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
+    // int n;
+    // cin >> n;
+    // int arr[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
 
-    int maxNO = INT_MIN;
-    int minNO = INT_MAX;
+    // int maxNO = INT_MIN;
+    // int minNO = INT_MAX;
 
-    for (int i = 0; i < n; i++)
-    {
-        maxNO = max(maxNO,arr[i]);
-        minNO = min(minNO,arr[i]);
-    }
-    cout<<maxNO<<minNO;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     maxNO = max(maxNO,arr[i]);
+    //     minNO = min(minNO,arr[i]);
+    // }
+    // cout<<maxNO<<minNO;
+    cout << "1";
     return 0;
 }
