7803280aa37cc1cd22db1570a116299c908f7aa7
Himanshu0422 <himanshumittal035@gmail.com>
2022-12-11 12:05:59 +0530

    kth Ancestor

---
+++ DSA/BinaryTress/kthancestor.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    } 
+};
+
+Node* solve(Node* root, int &k, int node) {
+    if(root == NULL)
+        return NULL;
+        
+    if(root->data == node) 
+    {
+        return root;
+    }
+    
+    Node* leftAns = solve(root->left, k, node);
+    Node* rightAns = solve(root->right, k, node);
+
+
+    if(leftAns != NULL && rightAns == NULL) 
+    {
+        k--;
+        if(k<=0) 
+        {
+            k = INT_MAX;
+            return root;
+        }
+        return leftAns;
+    }
+    
+    if(leftAns == NULL && rightAns != NULL) {
+        k--;
+        if(k<=0) 
+        {
+            k = INT_MAX;
+            return root;
+        }
+        return rightAns;
+    }
+    return NULL;
+}
+
+int kthAncestor(Node *root, int k, int node)
+{
+    Node* ans = solve(root, k, node);
+    if(ans == NULL || ans->data == node)
+        return -1;
+    else
+        return ans->data;
+}
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(5);
+    root->left->left = new Node(3);
+    root->left->right = new Node(4);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    root->right->left->right = new Node(8);
+    root->right->right->right = new Node(9);
+
+    
+    cout<<kthAncestor(root,1,4);
+
+    return 0;
+}
